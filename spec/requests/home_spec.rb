6d6c0f99462419ff330d637f3ba2b855973cdaf3
nasunasu26 <dkbk.lovelove@gmail.com>
2026-05-19 17:11:48 +0900

    テスト追加

--- spec/requests/home_spec.rb
+++ spec/requests/home_spec.rb
@@ -1,8 +1,30 @@
 require 'rails_helper'
 
-RSpec.describe "Home", type: :request do
+RSpec.describe "Pages", type: :request do
+
   it "トップページにアクセスできること" do
     get root_path
     expect(response).to have_http_status(:ok)
   end
+
+  it "自由演奏モードにアクセスできること" do
+    get free_play_path
+    expect(response).to have_http_status(:ok)
+  end
+
+  it "曲選択画面にアクセスできること" do
+    get learns_path
+    expect(response).to have_http_status(:ok)
+  end
+
+  it "きらきら星の練習ページにアクセスできること" do
+    get learn_path("kirakira")
+    expect(response).to have_http_status(:ok)
+  end
+  
+  it "クリア画面にアクセスできること" do
+    get "/learns/kirakira/clear", params: { title: "きらきら星" }
+    expect(response).to have_http_status(:ok)
+  end
+
 end
